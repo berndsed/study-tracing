223a77e2dcef61f293943e2e0d21412192e060e0
Bernhard <1688588+berndsed@users.noreply.github.com>
2019-10-07 11:11:26 +0200

    Enable propagation of span context for jax-rs-clients

--- app/src/main/java/de/kieseltaucher/studies/tracing/outbound/TimeOfDayClient.java
+++ app/src/main/java/de/kieseltaucher/studies/tracing/outbound/TimeOfDayClient.java
@@ -3,9 +3,9 @@ package de.kieseltaucher.studies.tracing.outbound;
 import java.util.logging.Logger;
 
 import javax.enterprise.context.Dependent;
+import javax.inject.Inject;
 import javax.ws.rs.ServiceUnavailableException;
 import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
 
 import de.kieseltaucher.studies.tracing.domain.TimeOfDay;
@@ -15,8 +15,10 @@ public class TimeOfDayClient {
 
     private final Logger logger = Logger.getLogger(getClass().getName());
 
+    @Inject
+    private Client client;
+
     public TimeOfDay getTimeOfDay() {
-        final Client client = ClientBuilder.newClient();
         try {
             return client.target("http://localhost:8080")
                 .path("/time-of-day")
