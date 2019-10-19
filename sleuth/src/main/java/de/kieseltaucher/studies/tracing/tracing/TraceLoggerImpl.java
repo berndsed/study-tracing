ebab0e9c5ce0bd2d44206a42bbd99259fd5df03b
Bernhard <1688588+berndsed@users.noreply.github.com>
2019-10-19 13:57:35 +0200

    Send events and errors to zipkin

--- sleuth/src/main/java/de/kieseltaucher/studies/tracing/tracing/TraceLoggerImpl.java
+++ sleuth/src/main/java/de/kieseltaucher/studies/tracing/tracing/TraceLoggerImpl.java
@@ -1,21 +1,29 @@
 package de.kieseltaucher.studies.tracing.tracing;
 
+import javax.inject.Inject;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
+import brave.Tracer;
 
 @Component
 class TraceLoggerImpl implements TraceLogger {
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+    @Inject
+    private Tracer tracer;
 
     @Override
     public void log(String msg) {
+        tracer.currentSpanCustomizer().annotate(msg);
         logger.info(msg);
     }
 
     @Override
     public void error(Exception e) {
+        tracer.currentSpanCustomizer().tag("error", e.toString());
         logger.error(e.toString(), e);
     }
 }
